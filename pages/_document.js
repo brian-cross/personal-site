6a853ffd9086ae5c1c555167cedf5a2f2f61c826
Brian Cross <briancross@shaw.ca>
2021-02-12 13:16:36 -0700

    Added dns-prefetch for Google fonts to head.

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="dns-prefetch" href="//fonts.googleapis.com"></link>
+        </Head>
         <body>
           <Main />
           <NextScript />
