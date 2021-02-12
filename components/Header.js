17a1ad1d49e786dced586a6112061d865171cabe
Brian Cross <briancross@shaw.ca>
2021-02-12 12:49:45 -0700

    Import m component instead of motion component from framer motion. Reduces bundle size.

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { m as motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { header, menuIcon } from "../styles/Header.module.scss";
 
