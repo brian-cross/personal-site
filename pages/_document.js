225b76f00d74d3128f71b0aa685747e110007e12
Brian Cross <briancross@shaw.ca>
2021-02-12 14:07:15 -0700

    Added link preconnect to head.

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="dns-prefetch" href="https://fonts.googleapis.com"></link>
-          <link rel="preload" href="https://fonts.googleapis.com"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         </Head>
         <body>
           <Main />
