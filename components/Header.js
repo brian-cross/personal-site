03bcc55adcc05a1c85a1a6a7837f89fbe4d18479
Brian Cross <briancross@shaw.ca>
2021-02-12 20:37:16 -0700

    Animation and styling changes.

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,38 @@
 import Link from "next/link";
-import { m as motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
-import { header, menuIcon } from "../styles/Header.module.scss";
+import {
+  header,
+  homelink,
+  menuIcon,
+  navlink,
+} from "../styles/Header.module.scss";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [enableAnimation, setEnableAnimation] = useState(false);
 
   useEffect(() => {
     const mediaQueryList = window.matchMedia("(min-width: 700px)");
-    const listener = e => setMenuOpen(e.matches);
+
+    setEnableAnimation(!mediaQueryList.matches);
     setMenuOpen(mediaQueryList.matches);
+
+    const listener = e => {
+      // Mobile -> Desktop
+      if (e.matches) {
+        setEnableAnimation(false);
+        setMenuOpen(true);
+      }
+      // Desktop -> Mobile
+      else {
+        setMenuOpen(false);
+        setEnableAnimation(true);
+      }
+    };
+
     mediaQueryList.addEventListener("change", listener);
+
     return () => mediaQueryList.removeEventListener("change", listener);
   }, []);
 
@@ -20,48 +42,86 @@ export default function Header() {
   };
 
   const menu = {
-    open: { clipPath: `polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)` },
-    closed: { clipPath: `polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)` },
+    open: {
+      clipPath: `polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)`,
+      transition: enableAnimation
+        ? {
+            delay: 0.3,
+            duration: 0.5,
+            delayChildren: 0.75,
+            staggerChildren: 0.1,
+          }
+        : { duration: 0 },
+    },
+    closed: {
+      clipPath: `polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)`,
+      transition: enableAnimation
+        ? {
+            delay: 0.75,
+            duration: 0.5,
+            delayChildren: 0.3,
+            staggerChildren: 0.1,
+            staggerDirection: -1,
+          }
+        : { duration: 0 },
+    },
   };
 
-  const transition = { type: "spring", duration: 0.5 };
+  const item = {
+    open: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "tween",
+        duration: enableAnimation ? 0.2 : 0,
+      },
+    },
+    closed: {
+      opacity: 0,
+      y: 10,
+      transition: {
+        type: "tween",
+        duration: enableAnimation ? 0.2 : 0,
+      },
+    },
+  };
 
   return (
     <header className={header}>
       <nav>
-        <h1>Brian Cross</h1>
+        <Link href="/">
+          <a className={homelink}>
+            <h1>Brian Cross</h1>
+          </a>
+        </Link>
         <motion.div
           variants={icon}
           animate={menuOpen ? "open" : "closed"}
-          transition={transition}
+          transition={{ type: "tween", duration: enableAnimation ? 0.3 : 0 }}
           className={menuIcon}
           onClick={() => setMenuOpen(!menuOpen)}
         />
-        <motion.ul
-          variants={menu}
-          animate={menuOpen ? "open" : "closed"}
-          transition={transition}
-        >
-          <li>
+        <motion.ul variants={menu} animate={menuOpen ? "open" : "closed"}>
+          <motion.li variants={item}>
             <Link href="#">
-              <a>Work</a>
+              <a className={navlink}>Work</a>
             </Link>
-          </li>
-          <li>
+          </motion.li>
+          <motion.li variants={item}>
             <Link href="#">
-              <a>Blog</a>
+              <a className={navlink}>Blog</a>
             </Link>
-          </li>
-          <li>
+          </motion.li>
+          <motion.li variants={item}>
             <Link href="#">
-              <a>About</a>
+              <a className={navlink}>About</a>
             </Link>
-          </li>
-          <li>
+          </motion.li>
+          <motion.li variants={item}>
             <Link href="#">
-              <a>Contact</a>
+              <a className={navlink}>Contact</a>
             </Link>
-          </li>
+          </motion.li>
         </motion.ul>
       </nav>
     </header>
