84dcbcca9797447603d69996c2085d61007fbefe
Brian Cross <briancross@shaw.ca>
2021-02-12 20:55:56 -0700

    Added meta description.

--- components/Layout.js
+++ components/Layout.js
@@ -12,6 +12,10 @@ export default function Layout({ children }) {
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         ></meta>
+        <meta
+          name="description"
+          content="I'm a web developer from Canada. I build lightning fast websites using modern frameworks such as React and Next.JS as well as vanilla JavaScript, HTML, and CSS."
+        ></meta>
       </Head>
       <div className={layout}>
         <Header />
