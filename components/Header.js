61bcedc4021e43d5d3410bd5e820d9b9c9a08ab5
Brian Cross <briancross@shaw.ca>
2021-02-14 15:30:26 -0700

    Implemented mobile nav menu animation in GSAP.

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,52 @@
 import Link from "next/link";
-import { useState } from "react";
+import gsap from "gsap";
+import { useEffect, useRef, useState } from "react";
 import {
   header,
   homelink,
   menuIcon,
-  navlink,
+  navLink,
+  navLinks,
 } from "../styles/Header.module.scss";
 
 export default function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [, setMenuOpen] = useState(false);
+
+  let menuButton = useRef(null);
+  let menu = useRef(null);
+  const timeline = useRef();
+
+  useEffect(() => {
+    const menuUp = "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)";
+    const menuDown = "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)";
+
+    timeline.current = gsap.timeline({
+      paused: true,
+      defaults: { duration: 0.3, ease: "power1.inOut" },
+    });
+
+    timeline.current
+      .fromTo(menuButton, { rotation: 0 }, { rotation: 180 }, 0)
+      .fromTo(
+        menu,
+        { clipPath: menuUp, visibility: "hidden" },
+        { clipPath: menuDown, visibility: "visible" },
+        0
+      )
+      .fromTo(
+        `.${menu.className} > li`,
+        { opacity: 0, y: "1em", stagger: 0.125 },
+        { opacity: 1, y: "0em", stagger: 0.125 }
+      );
+  }, []);
+
+  function handleMenuButton() {
+    setMenuOpen(prev => {
+      if (!prev) timeline.current.play();
+      else timeline.current.reverse();
+      return !prev;
+    });
+  }
 
   return (
     <header className={header}>
@@ -18,26 +56,30 @@ export default function Header() {
             <h1>Brian Cross</h1>
           </a>
         </Link>
-        <div className={menuIcon} onClick={() => setMenuOpen(!menuOpen)} />
-        <ul>
+        <div
+          className={menuIcon}
+          ref={el => (menuButton = el)}
+          onClick={handleMenuButton}
+        />
+        <ul className={navLinks} ref={el => (menu = el)}>
           <li>
             <Link href="#">
-              <a className={navlink}>Work</a>
+              <a className={navLink}>Work</a>
             </Link>
           </li>
           <li>
             <Link href="#">
-              <a className={navlink}>Blog</a>
+              <a className={navLink}>Blog</a>
             </Link>
           </li>
           <li>
             <Link href="#">
-              <a className={navlink}>About</a>
+              <a className={navLink}>About</a>
             </Link>
           </li>
           <li>
             <Link href="#">
-              <a className={navlink}>Contact</a>
+              <a className={navLink}>Contact</a>
             </Link>
           </li>
         </ul>
