ab7157961706eafa0b00a231f9ad117a2cc9762e
Brian Cross <brian.cross.1977@gmail.com>
2021-02-21 07:01:23 -0700

    Added theme file to work with styled jsx.

---
+++ styles/theme.js
@@ -0,0 +1,31 @@
+export const colors = {
+  white: "white",
+  black: "black",
+  lightGrey: "hsl(0, 0%, 75%)",
+  mediumGrey: "hsl(0, 0%, 50%)",
+  mediumDarkGrey: "hsl(0, 0%, 25%)",
+  darkGrey: "hsl(0, 0%, 15%)",
+  darkestGrey: "hsl(0, 0%, 7%)",
+};
+
+export const font = {
+  primary: colors.white,
+  secondary: colors.mediumGrey,
+  footer: colors.mediumGrey,
+};
+
+export const background = {
+  site: colors.black,
+  header: colors.darkGrey,
+  menu: colors.mediumGrey,
+};
+
+export const scrollbar = {
+  track: colors.darkestGrey,
+  thumb: colors.mediumGrey,
+};
+
+export const footer = {
+  font: colors.mediumGrey,
+  separator: colors.darkGrey,
+};
