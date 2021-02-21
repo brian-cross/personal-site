870824955c4581c9c483beef8be21b9c61f6e2dd
Brian Cross <brian.cross.1977@gmail.com>
2021-02-21 06:58:44 -0700

    Added global page transitions to app.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,41 @@
 import "normalize.css";
 import "../styles/globals.scss";
 import Layout from "../components/Layout";
+import { PageTransition } from "next-page-transitions";
 
-function MyApp({ Component, pageProps }) {
+const TIMEOUT = 500;
+
+function MyApp({ Component, pageProps, router }) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <PageTransition timeout={TIMEOUT} classNames="page-transition">
+        <Component {...pageProps} key={router.route} />
+      </PageTransition>
+      <style jsx global>
+        {`
+          [class^="page-transition"] {
+            height: 100%;
+          }
+
+          .page-transition-enter {
+            opacity: 0;
+          }
+
+          .page-transition-enter-active {
+            opacity: 1;
+            transition: opacity ${TIMEOUT}ms ease-in;
+          }
+
+          .page-transition-exit {
+            opacity: 0;
+            transition: opacity ${TIMEOUT}ms ease-out;
+          }
+
+          .page-transition-exit-active {
+            opacity: 0;
+          }
+        `}
+      </style>
     </Layout>
   );
 }
